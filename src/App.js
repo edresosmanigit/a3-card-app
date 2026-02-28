9397662252236bacddc4079fd0d68a7f0a76452e
edresosmanigit <edresosmanigit@gmail.com>
2026-02-27 20:20:16 -0500

    added test card object to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { buildDeck } from "./helpers";
+import Card from "./card";
 
 function App() {
   return (
     <div>
       <h1>Card App</h1>
+      <Card suit="Hearts" num="2" picked={true} onClick={() => {}} />
     </div>
   );
 }
