6925fe5bfd25a916a7b568ea141f1ddf364e4fa6
edresosmanigit <edresosmanigit@gmail.com>
2026-02-27 19:57:50 -0500

    created shuffleDeck function

--- src/helpers.js
+++ src/helpers.js
@@ -10,3 +10,12 @@ export function buildDeck(){
         }))
     );
 }
+
+export function shuffleDeck(cardArray){
+    const arr = [...cardArray];
+    for(let i = arr.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [a[j], a[i]];
+    }
+    return a;
+}
