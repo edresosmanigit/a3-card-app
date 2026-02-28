d6b493f32f74caf16a0b2ec66e46765d1ab9f8ca
edresosmanigit <edresosmanigit@gmail.com>
2026-02-27 20:13:18 -0500

    created card function

---
+++ src/card.js
@@ -0,0 +1,11 @@
+function Card({suit, num, picked, onClick}){
+    const redCards = suit == "Hearts" || suit =="Diamonds";
+    const blackCards = suit == "Spades" || suit =="Clubs";
+    const classes = `card ${redCards ? "red" : ""} ${blackCards ? "black" : ""} ${picked ? "picked" : ""}`
+
+    return(
+        <div className={classes} onClick={onClick}>
+            {num}{suit}
+        </div>
+    );
+}
