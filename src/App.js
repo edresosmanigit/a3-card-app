f075bd61583c8e4082471fdd4e64f80a5de299bd
edresosmanigit <edresosmanigit@gmail.com>
2026-02-27 20:43:37 -0500

    created drawOne function

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ function App() {
 	const [deck, createDeck] = useState(buildDeck());
 	const [hand, createHand] = useState([]);
 
+	function drawOne(){
+		if (deck.length == 0) return;
+		
+		const cardIndex = Math.floor(Math.random() * deck.length);
+		const card = deck[cardIndex];
+
+		createDeck(deck.filter((_, i) => i != cardIndex));
+		createHand([...hand, card])
+	}
+
   	return (
 		<div className="app">
 			<h1>Card App</h1>
