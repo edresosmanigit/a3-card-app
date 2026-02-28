a4b295f43c9a0cd53eb5619fe0a582ecbbdacce9
edresosmanigit <edresosmanigit@gmail.com>
2026-02-27 19:51:50 -0500

    created buildDeck function

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,12 @@
 export const SUITS = ["Spades", "Diamonds", "Clovers", "Hearts"];
 export const CARDNUMS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
+export function buildDeck(){
+    return SUITS.flatMap((suit) =>
+        CARDNUMS.map((nums) => ({
+            suit: suit,
+            nums: nums,
+            id: `${nums}${suit}`
+        }))
+    );
+}
