65d26e8249843438f6d5c4b1b499aa48b750d81c
edresosmanigit <edresosmanigit@gmail.com>
2026-02-27 20:37:58 -0500

    created initial state for deck and hand

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { buildDeck } from "./helpers";
+import { buildDeck} from "./helpers";
 import Card from "./card";
+import { use, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <h1>Card App</h1>
-      <Card suit="Hearts" num="2" picked={true} onClick={() => {}} />
-    </div>
-  );
+	const [deck, createDeck] = useState(buildDeck());
+	const [hand, createHand] = useState([]);
+
+  	return (
+		<div className="app">
+			<h1>Card App</h1>
+			<p>Deck: {deck.length} Hand: {hand.length}</p>
+		</div>
+  	);
 }
 
 export default App;
