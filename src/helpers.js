30ee2fcf14aa1359a1b68944f3c972d12f28f888
edresosmanigit <edresosmanigit@gmail.com>
2026-02-27 20:07:43 -0500

    fixed name of clubs suit

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-export const SUITS = ["Spades", "Diamonds", "Clovers", "Hearts"];
+export const SUITS = ["Spades", "Diamonds", "Clubs", "Hearts"];
 export const CARDNUMS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
 export function buildDeck(){
